84bd531a45ce1b12e0437331f54aac7845304172
Uwe Eichkorn <git.bsetunes@outlook.com>
2025-10-17 08:23:16 +0200

    assemply versions changed

--- legacy-projects/BSE.Platten/BSE.Platten.Ripper/Properties/AssemblyInfo.cs
+++ legacy-projects/BSE.Platten/BSE.Platten.Ripper/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BSE")]
 [assembly: AssemblyProduct("BSE.Platten.Ripper")]
-[assembly: AssemblyCopyright("Copyright © BSE 2008")]
+[assembly: AssemblyCopyright("Copyright © BSE 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.18")]
-[assembly: AssemblyFileVersion("1.0.0.18")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("de")]
